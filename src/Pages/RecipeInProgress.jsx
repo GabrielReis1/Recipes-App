1f72385fb7f7a1bd12ab9e187a43bbeed6312736
Vinícius Coelho <viniciusraposo@gmail.com>
2023-04-11 21:48:29 -0300

    merge

--- src/Pages/RecipeInProgress.jsx
+++ src/Pages/RecipeInProgress.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useParams, useHistory, useLocation } from 'react-router-dom';
-import ShareButton from '../Components/ShareButton';
-// import FavoriteButton from '../Components/FavoriteButton';
-import './recipeinprogress.css';
 import { LsDone, LsProgress } from '../Services/localStorageFuncs';
 import { useFilter } from '../Contexts/ProviderFilter';
+import ShareButton from '../Components/ShareButton';
 import FavoriteButton from '../Components/FavoriteButton';
+
+import './recipeinprogress.css';
+import '../Components/recipes.css';
 
 function RecipeInProgress() {
   const { pathname } = useLocation();
@@ -37,60 +38,68 @@ function RecipeInProgress() {
   }
 
   return (
-    <main>
-      <section>
+    <section className="details">
+      <article className="details-img-bg">
         <img
           src={ detailRecipes.strMealThumb || detailRecipes.strDrinkThumb }
           alt={ detailRecipes.strMeal || detailRecipes.strDrink }
           data-testid="recipe-photo"
-          className="recipe-photo"
+          className="details-img"
         />
-        <section className="recipe-details">
-          <div className="recipe-header">
-            <h1 data-testid="recipe-title">
-              { detailRecipes.strMeal || detailRecipes.strDrink }
+      </article>
 
-            </h1>
-            <ShareButton
-              type={ type }
-              id={ id }
-            />
-            <FavoriteButton
-              type={ type }
-              id={ id }
-              detailRecipes={ detailRecipes }
-            />
-          </div>
-          <h3 data-testid="recipe-category">{ detailRecipes.strCategory }</h3>
-          <section className="ingredients-list">
-            <h2>Ingredients</h2>
-            <ul>
-              { ingredients?.map((ingredient, idx) => (
-                <li
-                  data-testid={ `${idx}-ingredient-name-and-measure` }
-                  key={ idx }
-                >
-                  { detailRecipes[ingredient] }
-                  {' '}
-                  { detailRecipes[`strMeasure${idx + 1}`] }
-                </li>
-              ))}
-            </ul>
-          </section>
-          <section className="instructions">
-            <h2>Instructions</h2>
-            <p data-testid="instructions">{ detailRecipes.strInstructions }</p>
-          </section>
+      <header className="details-header">
+        <p data-testid="recipe-category" className="details-category">
+          { detailRecipes.Alcoholic === 'Alcoholic'
+            ? `${detailRecipes.strCategory} - ${detailRecipes.strAlcoholic}`
+            : detailRecipes.strCategory }
+        </p>
+      </header>
+      <section className="recipe-details">
+        <div className="recipe-header">
+          <h1 data-testid="recipe-title">
+            { detailRecipes.strMeal || detailRecipes.strDrink }
+
+          </h1>
+          <ShareButton
+            type={ type }
+            id={ id }
+          />
+          <FavoriteButton
+            type={ type }
+            id={ id }
+            detailRecipes={ detailRecipes }
+          />
+        </div>
+
+        <section className="ingredients-list">
+          <h2>Ingredients</h2>
+          <ul>
+            { ingredients?.map((ingredient, idx) => (
+              <li
+                data-testid={ `${idx}-ingredient-name-and-measure` }
+                key={ idx }
+              >
+                { detailRecipes[ingredient] }
+                {' '}
+                { detailRecipes[`strMeasure${idx + 1}`] }
+              </li>
+            ))}
+          </ul>
         </section>
-        <button
-          type="button"
-          data-testid="finish-recipe-btn"
-          onClick={ handleFinishRecipe }
-        >
-          Finish Recipe
-        </button>
+        <section className="instructions">
+          <h2>Instructions</h2>
+          <p data-testid="instructions">{ detailRecipes.strInstructions }</p>
+        </section>
       </section>
-    </main>
+      <button
+        type="button"
+        data-testid="finish-recipe-btn"
+        onClick={ handleFinishRecipe }
+      >
+        Finish Recipe
+      </button>
+    </section>
   );
 }
 
